0c2c240b3a449dfc523295f8eb44adaf4f780016
itongggg <itongggg@163.com>
2023-06-18 18:28:20 +0800

    final

--- ddpm2/ddpm_pytorch.py
+++ ddpm2/ddpm_pytorch.py
@@ -23,7 +23,7 @@ from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange
 from PIL import Image
 from tqdm.auto import tqdm
-# from ema_pytorch import EMA
+from ema_pytorch import EMA
 from accelerate import Accelerator
 from pytorch_fid.inception import InceptionV3
 from pytorch_fid.fid_score import calculate_frechet_distance
@@ -735,4 +735,149 @@ class Trainer:
         self.inception_v3 = None
 
         if calculate_fid:
-            assert inception_block_idx
+            assert inception_block_idx in InceptionV3.BLOCK_INDEX_BY_DIM
+            block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[inception_block_idx]
+            self.inception_v3 = InceptionV3([block_idx])
+            self.inception_v3.to(self.device)
+
+        # sampling and trainging hyperparameters
+        assert has_int_squareroot(num_samples), 'num_samples must be a square number'
+        self.num_samples = num_samples
+        self.save_and_sample_every = save_and_sample_every
+
+        self.batch_size = train_batch_size
+        self.gradient_accumulate_every = gradient_accumulate_every
+
+        self.train_num_steps = train_num_steps
+        self.image_size = diffusion_model.image_size
+
+        # dataset and dataloader
+
+
+
+
+        # optimizer
+        self.opt = Adam(self.model.parameters(), lr=train_lr, betas=adam_betas)
+
+        # for logging results in a folder periodically
+
+        if self.acceleator.is_main_process:
+            self.ema = EMA(diffusion_model, beta=ema_decay, update_every=ema_update_every)
+            self.ema.to(self.device)
+
+        self.results_folder = Path(results_folder)
+        self.results_folder.mkdir(exist_ok=True)
+
+        # step counter state
+        self.step = 0
+
+        # prepare model, dataloader, optimizer with accelerator
+
+        self.model, self.opt = self.acceleator.prepare(self.model, self.opt)
+
+    @property
+    def device(self):
+        return self.acceleator.device
+
+    def save(self, milestone):
+        if not self.acceleator.is_main_process:
+            return
+        data = {
+                'model': self.model.get_state_dict(self.model),
+                'opt': self.opt.state_dict(),
+                'ema': self.ema.state_dict(),
+                'step': self.step,
+                'scaler': self.acceleator.scaler.state_dict() if exists(self.acceleator.scaler) else None,
+            }
+
+        torch.save(data, str(self.results_folder / f'model_{milestone}.pt'))
+
+    def load(self, milestone):
+        accelerator = self.acceleator
+        device = self.device
+
+        data = torch.load(str(self.results_folder / f'model_{milestone}.pt'), map_location=device)
+        model = self.acceleator.unwrap_model(self.model)
+        model.load_state_dict(data['model'])
+
+        self.step = data['step']
+        self.opt.load_state_dict(data['opt'])
+        if self.acceleator.is_main_process:
+            self.ema.load_state_dict(data['ema'])
+        
+        if exists(data['scaler']) and exists(self.acceleator.scaler):
+            self.acceleator.scaler.load_state_dict(data['scaler'])
+
+    @torch.no_grad()
+    def calculate_activation_statistics(self, samples):
+        assert exists(self.inception_v3), 'must set calculate_fid=True to use this function'
+
+        features = self.inception_v3(samples)[0]
+        features = rearrange(features, '... 1 1 -> ...').cpu().numpy()
+        mu = np.mean(features, axis=0)
+        sigma = np.cov(features, rowvar=False)
+        return mu, sigma
+
+    def fid_score(self, real_samples, fake_samples):
+        if self.channels == 1:
+            real_samples, fake_samples = map(lambda t: repeat(t, 'b 1 ... -> b c ... ', c=3), (real_samples, fake_samples))
+
+        min_batch = min(real_samples.shape[0], fake_samples.shape[0])
+        real_samples, fake_samples = real_samples[:min_batch], fake_samples[:min_batch]
+
+        m1, s1 = self.calculate_activation_statistics(real_samples)
+        m2, s2 = self.calculate_activation_statistics(fake_samples)
+
+        fid = calculate_frechet_distance(m1, s1, m2, s2)
+        return fid
+
+    def train(self):
+        accelerator = self.acceleator
+        device = accelerator.device
+
+        with tqdm(initial=self.step, total=self.train_num_steps, disable=not accelerator.is_main_process) as pbar:
+            while self.step < self.train_num_steps:
+                total_loss = 0.
+
+                for _ in range(self.gradient_accumulate_every):
+                    data = next(self.dl).to(device)
+
+                    with self.acceleator.autocast():
+                        loss = self.model(data)
+                        loss = loss / self.gradient_accumulate_every
+                        total_loss += loss.item()
+
+                    self.acceleator.backward(loss)
+
+                accelerator.clip_grad_norm_(self.model.parameters(), 1.)
+                pbar.set_description(f'loss: {total_loss:.4f}')
+
+                accelerator.wait_for_everyone()
+
+                self.opt.step()
+                self.opt.zero_grad()
+                accelerator.wait_for_everyone()
+                self.step += 1
+                if accelerator.is_main_process:
+                    self.ema.update()
+
+                    if self.step != 0 and self.step % self.save_and_sample_every == 0:
+                        self.ema.ema_model().eval()
+
+                        with torch.no_grad():
+                            milestone = self.step // self.save_and_sample_every
+                            batches = num_to_groups(self.num_samples, self.batch_size)
+                            all_images_list = list(map(lambda n: self.ema.ema_model.sample(batch_size=n), batches))
+                        
+                        all_images = torch.cat(all_images_list, dim=0)
+                        utils.save_image(all_images, str(self.results_folder / f'sample_{milestone}.png'), nrow=int(sqrt(self.num_samples)))
+                        self.save(milestone)
+
+                        # whether to calculate fid
+                        if exists(self.inception_v3):
+                            fid_score = self.fid_score(data, all_images)
+                            accelerator.print(f'fid score: {fid_score}')
+
+                pbar.update(1)
+        accelerator.print('training completed')
+
